63b2af06de4c6f922c94e7bc49d3bb99daa8bc2c
Theodora2000 <xsimont@stuba.sk>
2021-03-29 20:56:50 +0200

    5.3.3

--- src/main/java/sk/stuba/uim/fei/oop/TestCanvas.java
+++ src/main/java/sk/stuba/uim/fei/oop/TestCanvas.java
@@ -7,22 +7,19 @@ public class TestCanvas extends Canvas{
         Dimension d = getSize();
         int w = d.width;
         int h = d.height;
-/*
-        g.drawLine(0,0,w-1,h-1);
-        g.drawLine(0,h-1,w-1,0);//zlava dole doprava dole
-        g.drawRect(0,0,w-1,h-1);*/
 
-        int i=1;
-        Color C;
-        while(i < w-1 - i  && i < h-1 - i){
-            if(2*i < 256){
-                C = new Color(2*i,255 - 2*i,255);
-            }else{
-                C = new Color(255,0,255);
-            }
-            g.setColor(C);
-            g.drawRect(i,i,w-1-2*i, h-1-2*i);
-            i++;
-        }
+        g.setColor(Color.RED);
+        g.fillRect(10, 100, 100, 100);
+        g.setColor(Color.BLACK);
+        g.drawRect(10, 100, 100, 100);
+
+        g.setColor(Color.RED);
+        g.fillOval(120, 200, 100, 100);
+        g.setColor(Color.BLACK);
+        g.drawOval(120, 200, 100, 100);
+
+
+
+
     }
 }
